24e1f53b608bbe147fd6691961c8e9087fb42813
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-25 23:49:40 +0300

    Update ru.js

--- ru.js
+++ ru.js
@@ -1,4 +1,4 @@
-onst Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const ytdl = require("ytdl-core");
 const { Client, Util } = require('discord.js');
